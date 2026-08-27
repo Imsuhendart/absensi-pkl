b9268145f4a7832004658a43d7dbd8243cefdd12
imsuhendart <imsuhendart@gmail.com>
2026-08-27 10:24:55 +0700

    Fix ejs path resolution in vercel serverless

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
-const app = require('../app');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set lokasi views menggunakan path absolute proyek
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+// Import app utama Anda
+const mainApp = require('../app');
+app.use('/', mainApp);
 
 module.exports = app;
